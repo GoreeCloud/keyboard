02495ef25b8414cf9293b37dbd75cb04d5f3ac63
GoreeCloud <295260680+GoreeCloud@users.noreply.github.com>
2026-09-25 15:50:44 -0500

    Bump Keyboard Development package to 0.1.1-dev

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.goreecloud.keyboard"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0-dev"
+        versionCode = 2
+        versionName = "0.1.1-dev"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
